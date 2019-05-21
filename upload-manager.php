49db244acdfbeaae07bd3975f3fa4c8e8f0303b0
Your Name <tmaxsolutions@gmail.com>
2019-05-21 09:47:17 -0500

    Excel Upload function fixed errors

--- upload-manager.php
+++ upload-manager.php
@@ -38,7 +38,7 @@ else
                 
                 move_uploaded_file($_FILES["photo"]["tmp_name"], "upload/" . $filename);
               //echo  $_FILES['photo']['error'];
-                echo "Your file was uploaded successfully,".$Filename;
+                echo "Your file was uploaded successfully,".$filename;
             } 
         } else{
             echo "E1400 : There was a problem uploading your file. Please try again."; 
