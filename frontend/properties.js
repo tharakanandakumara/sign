295c97acf3f7cf1d81fc73333f1386b40ba9e1ba
Your Name <tmaxsolutions@gmail.com>
2019-06-15 11:54:56 -0500

    Front End V 1.0

--- frontend/properties.js
+++ frontend/properties.js
@@ -1,6 +1,6 @@
 gOptions = {
   // This can have nested stuff, arrays, etc.
-  serverUrl: 'http://ec2-18-212-57-171.compute-1.amazonaws.com:3000',
+  serverUrl: 'http://ec2-18-212-57-171.compute-1.amazonaws.com',
  
   sererPort:'3000'
 }
