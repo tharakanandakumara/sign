ac7f953c0d6b2cb3a1654f2c2ee2813f5fd3a2fc
Your Name <tmaxsolutions@gmail.com>
2019-06-16 00:55:32 -0500

    Front End v 1.1

--- frontend/add_students.php
+++ frontend/add_students.php
@@ -88,11 +88,142 @@ header("location: login.html");
             </div>
 
             <form id="studentreg" enctype="multipart/form-data">
-                                  <div class="form-group">
-                        <input name="indexNo" type="number" class="form-control" placeholder="Index No *" value="" required>
+                <div class="tab-content" id="myTabContent">
+                    <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+                        <div class="notify_panel3"></div>
+                        <h3 class="register-heading">Student Registration</h3>
+                        <div class="row register-form">
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <input name="indexNo" type="number" class="form-control" placeholder="Index No *" value="" required>
+                                </div>
+                                <div class="form-group">
+                                    <input name="initials" type="text" class="form-control" placeholder="Initials *" value="" required>
+                                </div>
+                                <div class="form-group">
+                                    <input name="lastName" type="text" class="form-control" placeholder="Last Name *" value="" required>
+                                </div>
+                                <div class="form-group">
+                                    <input name="fullName" type="text" class="form-control" placeholder="Full Name *" value="" required>
+                                </div>
+
+                                <div class="form-group">
+                                    <input type="file" name="photo" id="fileSelect" accept="image/*">
+                                </div>
+                                <div class="form-group">
+                                    <label for="dob">DOB</label>
+                                    <input name="DOB" id="dob " type="date" class="form-control" value="" required>
+                                </div>
+                                <div class="form-group">
+                                    <input name="address" type="text" class="form-control" placeholder="Adress *" value="" required>
+                                </div>
+                                <div class="form-group">
+                                    <div class="maxl">
+                                        <label class="radio inline">
+                                            <input type="radio" name="gender" value="male" checked>
+                                            <span> Male </span>
+                                        </label>
+                                        <label class="radio inline">
+                                            <input type="radio" name="gender" value="female">
+                                            <span>Female </span>
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="row">
+                                    <div class="col-md-4">
+                                        <select name="grade" class="form-control" required>
+                                            <option value="" class="hidden" selected disabled> Grade</option>
+                                            <option value="1">1</option>
+                                            <option value="2">2</option>
+                                            <option value="3">3</option>
+                                            <option value="4">4</option>
+                                            <option value="5">5</option>
+                                            <option value="6">6</option>
+                                            <option value="7">7</option>
+                                            <option value="8">8</option>
+                                            <option value="9">9</option>
+                                            <option value="10">10</option>
+                                            <option value="11">11</option>
+                                            <option value="12">12</option>
+                                            <option value="13">13</option>
+                                        </select>
+                                    </div>
+                                    <div class="col-md-8">
+                                        <div class="form-group">
+                                            <input name="class" type="text" class="form-control" placeholder="Section/Class" value="" required>
+                                        </div>
+                                    </div>
+
+                                </div>
+                                <div class="form-group">
+                                    <select name="medium" class="form-control" required>
+                                        <option value="" class="hidden" selected disabled> Medium</option>
+                                        <option value="Sinhala">Sinhala</option>
+                                        <option value="English">English</option>
+                                    </select>
+                                </div>
+                                <div class="form-group">
+                                    <input type="number" minlength="10" maxlength="10" name="homeTel" class="form-control" placeholder="Home Tel *" value="" required>
+                                </div>
+                                <div class="form-group">
+                                    <input name="guardianName" type="text" class="form-control" placeholder="Guardian Name" value="">
+                                </div>
+                                <div class="form-group">
+                                    <input name="guardianAddress" type="text" class="form-control" placeholder="Guardian Adress" value="">
+                                </div>
+                                <div class="form-group">
+                                    <input name="guardianRelationship" type="text" class="form-control" placeholder="Guardian Relationship" value="">
+                                </div>
+                                <div class="form-group">
+                                    <input name="guardianContact" type="text" class="form-control" placeholder="Guardian Contact" value="">
+                                </div>
+
+                                <input type="submit" class="btnRegister" value="Register" />
+                            </div>
+                        </div>
                     </div>
-                    <input type="file" name="photo" id="fileSelect" accept="image/*">
-                    <input type="submit">
+                    <div class="tab-pane fade show" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+                        <h3 class="register-heading">Apply as a Hirer</h3>
+                        <div class="row register-form">
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <input type="text" class="form-control" placeholder="First Name *" value="" />
+                                </div>
+                                <div class="form-group">
+                                    <input type="text" class="form-control" placeholder="Last Name *" value="" />
+                                </div>
+                                <div class="form-group">
+                                    <input type="email" class="form-control" placeholder="Email *" value="" />
+                                </div>
+                                <div class="form-group">
+                                    <input type="text" maxlength="10" minlength="10" class="form-control" placeholder="Phone *" value="" />
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <input type="password" class="form-control" placeholder="Password *" value="" />
+                                </div>
+                                <div class="form-group">
+                                    <input type="password" class="form-control" placeholder="Confirm Password *" value="" />
+                                </div>
+                                <div class="form-group">
+                                    <select class="form-control">
+                                        <option class="hidden" selected disabled>Please select your Sequrity Question</option>
+                                        <option>What is your Birthdate?</option>
+                                        <option>What is Your old Phone Number</option>
+                                        <option>What is your Pet Name?</option>
+                                    </select>
+                                </div>
+                                <div class="form-group">
+                                    <input type="text" class="form-control" placeholder="`Answer *" value="" />
+                                </div>
+                                <input type="submit" class="btnRegister" value="Register" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </form>
 
 
@@ -147,7 +278,7 @@ header("location: login.html");
                     contentType: false,
                     cache: false,
                     processData: false,
-                    data: data,
+                    data:  new FormData(studentreg),
 
                     // Update Url
                     success: function(response) { // Setting Token
