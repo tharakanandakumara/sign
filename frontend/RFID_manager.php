d2588ccbf15d9c3b4c891a49795c8c966d0cd2e4
Your Name <tmaxsolutions@gmail.com>
2019-06-16 19:41:43 -0500

    Front End v 1.1

--- frontend/RFID_manager.php
+++ frontend/RFID_manager.php
@@ -26,16 +26,16 @@
     <link rel="stylesheet" href="vendors/flag-icon-css/css/flag-icon.min.css">
     <link rel="stylesheet" href="vendors/selectFX/css/cs-skin-elastic.css">
     <link rel="stylesheet" href="vendors/jqvmap/dist/jqvmap.min.css">
-    
 
 
-        <link rel="stylesheet" href="assets/css/style.css">
+
+    <link rel="stylesheet" href="assets/css/style.css">
     <link rel="stylesheet" href="assets/css/style_registration.css">
 
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800' rel='stylesheet' type='text/css'>
-    
 
-       
+
+
 
 </head>
 
@@ -76,53 +76,53 @@
 
         <div class="content mt-3">
             <div class="col-md-6 offset-md-3">
-                <div class="card" style="width: 18rem;">
-  <img class="card-img-top" src="..." alt="Card image cap">
-  <div class="card-body">
-    <h5 class="card-title">Rfid Manager</h5>
-    <p class="card-text">Student should be added first.</p>
-    <form id="rfid">
-                     <div class="notify_panel3"></div>
-                    <div class="form-group">
-                        <input id="indexno" name="indexNo" type="number" class="form-control" placeholder="Index No *" value="" required>
+                <div class="card">
+
+                    <div class="card-body">
+                        <h5 class="card-title">Rfid Manager</h5>
+                        <p class="card-text">Student should be added first.</p>
+                        <form id="rfid">
+                            <div class="notify_panel3"></div>
+                            <div class="form-group">
+                                <input id="indexno" name="indexNo" type="number" class="form-control" placeholder="Index No *" value="" required>
+                            </div>
+                            <div class="form-group">
+                                <input id="rfidval" name="rfid" type="text" class="form-control" placeholder="rfid *" value="" required>
+                            </div>
+                            <input type="submit" class="btn-success" value="Add" />
+                        </form>
+
                     </div>
-                    <div class="form-group">
-                        <input id="rfidval" name="rfid" type="text" class="form-control" placeholder="rfid *" value="" required>
-                    </div>
-                    <input type="submit" class="btn-success" value="Add" />
-                </form>
-      
-  </div>
-</div>
-                
-                
-                
+                </div>
+
+
+
             </div>
         </div><!-- /#right-panel -->
 
         <!-- Right Panel -->
 
         <script src="../js/jquerymin.js"></script>
-            <script type="text/javascript" src="../js/noty.min.js"></script>
+        <script type="text/javascript" src="../js/noty.min.js"></script>
         <script src="vendors/jquery/dist/jquery.min.js"></script>
-    <script src="vendors/popper.js/dist/umd/popper.min.js"></script>
-    <script src="vendors/bootstrap/dist/js/bootstrap.min.js"></script>
-    <script src="assets/js/main.js"></script>
+        <script src="vendors/popper.js/dist/umd/popper.min.js"></script>
+        <script src="vendors/bootstrap/dist/js/bootstrap.min.js"></script>
+        <script src="assets/js/main.js"></script>
 
 
-    <script src="vendors/chart.js/dist/Chart.bundle.min.js"></script>
-    <script src="assets/js/dashboard.js"></script>
-    <script src="assets/js/widgets.js"></script>
-    <script src="vendors/jqvmap/dist/jquery.vmap.min.js"></script>
-    <script src="vendors/jqvmap/examples/js/jquery.vmap.sampledata.js"></script>
-    <script src="vendors/jqvmap/dist/maps/jquery.vmap.world.js"></script>
-        
-        
+        <script src="vendors/chart.js/dist/Chart.bundle.min.js"></script>
+        <script src="assets/js/dashboard.js"></script>
+        <script src="assets/js/widgets.js"></script>
+        <script src="vendors/jqvmap/dist/jquery.vmap.min.js"></script>
+        <script src="vendors/jqvmap/examples/js/jquery.vmap.sampledata.js"></script>
+        <script src="vendors/jqvmap/dist/maps/jquery.vmap.world.js"></script>
+
+
         <script>
             $("#rfid").submit(function(event) {
                 $index = $("#indexno").val();
-                $rfidval=$("#rfidval").val();
-                $token="BEARER eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7Im5hbWUiOiJJc3VydSBSdWh1bmFnZSIsInVzZXJuYW1lIjoiYWRtaW4xIiwiZW1haWwiOiJpc3VydS5ydWh1QGdtYWlsLmNvbSIsImNvbnRhY3QiOiIwNzc3MTExMjIyIn0sImlhdCI6MTU1OTg4MDY1NiwiZXhwIjoxNTU5ODg3ODU2fQ.QIOx4m_Kk32oXHtqXfaeOb5vmeMKYfXRUo4o9Cr9qUI";
+                $rfidval = $("#rfidval").val();
+                $token = "BEARER eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7Im5hbWUiOiJJc3VydSBSdWh1bmFnZSIsInVzZXJuYW1lIjoiYWRtaW4xIiwiZW1haWwiOiJpc3VydS5ydWh1QGdtYWlsLmNvbSIsImNvbnRhY3QiOiIwNzc3MTExMjIyIn0sImlhdCI6MTU1OTg4MDY1NiwiZXhwIjoxNTU5ODg3ODU2fQ.QIOx4m_Kk32oXHtqXfaeOb5vmeMKYfXRUo4o9Cr9qUI";
                 if ($index == null) {
 
 
@@ -131,17 +131,17 @@
                     event.preventDefault();
 
 
-                    var jsonData='{"rfid":"'+$rfidval+'"}'
+                    var jsonData = '{"rfid":"' + $rfidval + '"}'
                     //var data = JSON.stringify(jsonData);
                     console.log("data: " + jsonData)
-                    
-                   var value= HttpManager(jsonData,"PUT",'http://ec2-18-234-208-163.compute-1.amazonaws.com:3000/protected/students/' + $index,$token)
-                    if(value=="404"){
-                         console.log("VALUE    "+ value);
-                         notifyMe('.notify_panel3', 'User not added : User Cannot Found', '0');
-                }
+
+                    var value = HttpManager(jsonData, "PUT", 'http://ec2-18-234-208-163.compute-1.amazonaws.com:3000/protected/students/' + $index, $token)
+                    if (value == "404") {
+                        console.log("VALUE    " + value);
+                        notifyMe('.notify_panel3', 'User not added : User Cannot Found', '0');
                     }
-                  
+                }
+
             });
 
             (function($) {
@@ -160,77 +160,79 @@
                     normalizeFunction: 'polynomial'
                 });
             })(jQuery);
-function HttpManager($passed_data,$method,$url,$token){
-    
-            var data = $passed_data;
-            console.log("data:5555 " + data)
-            var response_to="";
-            $.ajax({
-                type: $method,
-                url: $url,
-               
-                data: data,
-                dataType: 'json',
-                contentType: 'application/json;charset=UTF-8',
-                async:false,
-                headers: {
-                    Authorization: $token
-                },
-                success: function(response) {
-                    if (response != null) {
-                        return response;
-                    }
-                    //console.log("Success: ", msg)
 
-                },
-                statusCode: {
-                404: function() {
-                    console.log("here");
-                    //notifyMe('.notify_panel', 'Invalid Username', '0');
-                    response_to= "404";
-                },
-                401: function() {
-                    //notifyMe('.notify_panel', 'Invalid password', '0');
-                    response_to= "401";
-                },
-                error: function(err) {
-                    if (err.message != null) {
-                        notifyMe('.notify_panel2', 'Student Exist', '1');
-                    }
-                    response_to= "error";
-                }
-            }
-            });
-    return response_to;
-    
-}
-   function notifyMe($classname, $message, $status) {
+            function HttpManager($passed_data, $method, $url, $token) {
 
-            if ($status == "1") {
-                statusnew = 'success';
-            } else {
-                statusnew = 'error';
-            }
-            console.log(statusnew);
-            var n = $($classname).noty({
-                text: $message,
-                theme: 'metroui',
+                var data = $passed_data;
+                console.log("data:5555 " + data)
+                var response_to = "";
+                $.ajax({
+                    type: $method,
+                    url: $url,
 
-                type: statusnew,
-
-
-                animation: {
-                    open: {
-                        height: 'toggle'
+                    data: data,
+                    dataType: 'json',
+                    contentType: 'application/json;charset=UTF-8',
+                    async: false,
+                    headers: {
+                        Authorization: $token
                     },
-                    close: {
-                        height: 'toggle'
+                    success: function(response) {
+                        if (response != null) {
+                            return response;
+                        }
+                        //console.log("Success: ", msg)
+
                     },
-                    speed: 500
+                    statusCode: {
+                        404: function() {
+                            console.log("here");
+                            //notifyMe('.notify_panel', 'Invalid Username', '0');
+                            response_to = "404";
+                        },
+                        401: function() {
+                            //notifyMe('.notify_panel', 'Invalid password', '0');
+                            response_to = "401";
+                        },
+                        error: function(err) {
+                            if (err.message != null) {
+                                notifyMe('.notify_panel2', 'Student Exist', '1');
+                            }
+                            response_to = "error";
+                        }
+                    }
+                });
+                return response_to;
+
+            }
+
+            function notifyMe($classname, $message, $status) {
+
+                if ($status == "1") {
+                    statusnew = 'success';
+                } else {
+                    statusnew = 'error';
                 }
-            });
-            n.setTimeout(3000);
-        }
+                console.log(statusnew);
+                var n = $($classname).noty({
+                    text: $message,
+                    theme: 'metroui',
+
+                    type: statusnew,
+
+
+                    animation: {
+                        open: {
+                            height: 'toggle'
+                        },
+                        close: {
+                            height: 'toggle'
+                        },
+                        speed: 500
+                    }
+                });
+                n.setTimeout(3000);
+            }
 
         </script>
 
