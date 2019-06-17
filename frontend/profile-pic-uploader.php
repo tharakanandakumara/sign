5067dfa52461cfe2302fe91e94661a13bccf308f
Your Name <tmaxsolutions@gmail.com>
2019-06-17 10:20:27 -0500

    Front End v 1.1

---
+++ frontend/profile-pic-uploader.php
@@ -0,0 +1,193 @@
+<?php
+
+?>
+
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js" lang="en">
+<!--<![endif]-->
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Sufee Admin - HTML5 Admin Template</title>
+    <meta name="description" content="Sufee Admin - HTML5 Admin Template">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="apple-touch-icon" href="apple-icon.png">
+    <link rel="shortcut icon" href="favicon.ico">
+
+    <link rel="stylesheet" href="vendors/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="vendors/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="vendors/themify-icons/css/themify-icons.css">
+    <link rel="stylesheet" href="vendors/flag-icon-css/css/flag-icon.min.css">
+    <link rel="stylesheet" href="vendors/selectFX/css/cs-skin-elastic.css">
+    <link rel="stylesheet" href="vendors/jqvmap/dist/jqvmap.min.css">
+
+
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/style_registration.css">
+
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800' rel='stylesheet' type='text/css'>
+
+</head>
+
+<body>
+
+
+    <!-- Left Panel -->
+    <?php include 'includes/left-menu.php' ?>
+    <!-- Left Panel -->
+
+    <!-- Right Panel -->
+
+    <div id="right-panel" class="right-panel">
+
+        <!-- Header-->
+        <?php include 'includes/header.php' ?>
+        <!-- /header -->
+        <!-- Header-->
+
+        <div class="breadcrumbs">
+            <div class="col-sm-4">
+                <div class="page-header float-left">
+                    
+                    <div class="page-title">
+                        <h1>Profile Picture Uploader</h1>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-8">
+                <div class="page-header float-right">
+                    <div class="page-title">
+                        <ol class="breadcrumb text-right">
+                            <li class="active">Dashboard</li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="content mt-3">
+            <div class="col-md-6 offset-md-3">
+                <div class="notify_panel"></div>
+                <form id ="profilePic"  enctype="multipart/form-data">
+                    <div class="form-group">
+                        <input name="indexNo" type="number" class="form-control" placeholder="Index No *" value="" required>
+                    </div>
+                    <input type="file" name="photo" id="fileSelect" accept="image/*">
+                    <input type="submit">
+                </form>
+            </div>
+        </div><!-- /#right-panel -->
+
+        <!-- Right Panel -->
+    <!-- Right Panel -->
+     
+ <script src="../js/jquerymin.js"></script>
+        
+           <script type="text/javascript" src="js/noty.min.js"></script>
+        <script src="assets/js/main.js"></script>
+
+
+       
+        
+        
+       
+       
+        
+        <script>
+        $('#profilePic').submit(function(e) {
+        e.preventDefault();
+
+
+        $.ajax({
+            type: "POST",
+            url: "image_upload_manager.php",
+            contentType: false,
+            cache: false,
+            processData: false,
+            data: new FormData(this),
+            // Update Url
+            success: function(response) { // Setting Token
+
+                if (response) {
+                    if (response == "Your file was uploaded successfully") {
+                        notifyMe('.notify_panel', response, '1');
+                        
+                    } else {
+                        var res=response.split(':');
+                        notifyMe('.notify_panel', res[1], '0');
+                        console.log(response);
+                        //error notification here
+                    }
+
+                } else {
+                    // notifyMe('.notify_panel', 'Invalid Credentials Entered', '0');
+                }
+            },
+            statusCode: {
+                404: function() {
+                    //notifyMe('.notify_panel', 'Invalid Username', '0');
+                },
+                401: function() {
+                    //notifyMe('.notify_panel', 'Invalid password', '0');
+                }
+            }
+        });
+        //console.log("token : " + token);
+
+        return false;
+    });
+            (function($) {
+                "use strict";
+
+                jQuery('#vmap').vectorMap({
+                    map: 'world_en',
+                    backgroundColor: null,
+                    color: '#ffffff',
+                    hoverOpacity: 0.7,
+                    selectedColor: '#1de9b6',
+                    enableZoom: true,
+                    showTooltip: true,
+                    values: sample_data,
+                    scaleColors: ['#1de9b6', '#03a9f5'],
+                    normalizeFunction: 'polynomial'
+                });
+            })(jQuery);
+            function notifyMe($classname, $message, $status) {
+
+            if ($status == "1") {
+                statusnew = 'success';
+            } else {
+                statusnew = 'error';
+            }
+
+            var n = $($classname).noty({
+                text: $message,
+                theme: 'metroui',
+
+                type: statusnew,
+
+
+                animation: {
+                    open: {
+                        height: 'toggle'
+                    },
+                    close: {
+                        height: 'toggle'
+                    },
+                    speed: 500
+                }
+            });
+            n.setTimeout(3000);
+        }
+
+        </script>
+
+</body>
+
+</html>
