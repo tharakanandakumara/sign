9ffa0c16d4bdaca75a570ca040ee2a590a8d0c6a
ShanikaEdiriweera <shanika.13@cse.mrt.ac.lk>
2019-09-24 09:03:15 +0530

    prod props

--- frontend/properties.js
+++ frontend/properties.js
@@ -1,11 +1,11 @@
 gOptions = {
   // prod
-  // serverUrl: 'http://ec2-18-234-208-163.compute-1.amazonaws.com:3000',
-  // clientUrl: 'http://ec2-18-234-208-163.compute-1.amazonaws.com',
+  serverUrl: 'http://ec2-18-234-208-163.compute-1.amazonaws.com:3000',
+  clientUrl: 'http://ec2-18-234-208-163.compute-1.amazonaws.com',
 
   // testing
-  serverUrl: 'http://ec2-18-212-57-171.compute-1.amazonaws.com:3000',
-  clientUrl: 'http://ec2-18-212-57-171.compute-1.amazonaws.com',
+  // serverUrl: 'http://ec2-18-212-57-171.compute-1.amazonaws.com:3000',
+  // clientUrl: 'http://ec2-18-212-57-171.compute-1.amazonaws.com',
 
   // localhost
   // serverUrl: 'http://127.0.0.1:3000',
