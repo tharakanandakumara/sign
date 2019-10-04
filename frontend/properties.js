e10a4a5ce05d94fedc608d20f881321a5237a526
ShanikaEdiriweera <shanika.13@cse.mrt.ac.lk>
2019-10-04 07:48:05 +0530

    add properties

--- frontend/properties.js
+++ frontend/properties.js
@@ -1,15 +1,6 @@
 gOptions = {
-  // prod
-  // serverUrl: 'http://ec2-18-234-208-163.compute-1.amazonaws.com:3000',
-  // clientUrl: 'http://ec2-18-234-208-163.compute-1.amazonaws.com',
-
-  // testing
-  serverUrl: 'http://ec2-18-212-57-171.compute-1.amazonaws.com:3000',
-  clientUrl: 'http://ec2-18-212-57-171.compute-1.amazonaws.com',
-
-  // localhost
-  // serverUrl: 'http://127.0.0.1:3000',
-  // clientUrl: 'http://127.0.0.1/sign',
+  serverUrl: 'http://127.0.0.1:3000',
+  clientUrl: 'http://127.0.0.1/sign',
 
   serverPort: ':3000'
 }
