efdfec9344bae11699ea0073dff6e44ef3969688
ShanikaEdiriweera <shanika.13@cse.mrt.ac.lk>
2019-10-05 23:14:34 +0530

    props

--- frontend/properties.js
+++ frontend/properties.js
@@ -1,11 +1,11 @@
 gOptions = {
   // testing
   serverUrl: 'http://ec2-18-212-57-171.compute-1.amazonaws.com:3000',
-  // clientUrl: 'http://ec2-18-212-57-171.compute-1.amazonaws.com',
+  clientUrl: 'http://ec2-18-212-57-171.compute-1.amazonaws.com',
 
   // localhost
   // serverUrl: 'http://127.0.0.1:3000',
-  clientUrl: 'http://127.0.0.1/sign',
+  // clientUrl: 'http://127.0.0.1/sign',
 
   serverPort: ':3000'
 }
