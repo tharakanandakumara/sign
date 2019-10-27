597138e3ba21f7e56947012b9cd6fca86dfe364c
Megha Kumari <meghajais.mj@gmail.com>
2019-10-27 13:33:40 +0530

    Made the final changes

--- frontend/class-monthly-report.php
+++ frontend/class-monthly-report.php
@@ -220,7 +220,7 @@ if (!isset($_SESSION['token'])) {
                     var year = $('#year option:selected').text();
                     var grade = $('#grade option:selected').text();
                     var selectedClass = $('#classDropdown option:selected').text();
-                    var month = parseInt(month) < 10 ? "0" + (parseInt(month) + 1) : (parseInt(month) + 1);
+                    var month = parseInt(month) < 9 ? "0" + (parseInt(month) + 1) : (parseInt(month) + 1);
                     var fromDate = "" + year + "-" + month + "-01";
                     var toDate = "" + year + "-" + month + "-31";
                     console.log("fromDate: ", fromDate);
