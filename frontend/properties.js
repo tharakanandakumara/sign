781dbe7a8d44f79c2cc56c2ddbfa9342f6c519ac
jk180my <jk180my@student.tuke.sk>
2019-11-04 10:53:50 +0100

    Student list + Student Profile implementation for Student Attendance Report

--- frontend/properties.js
+++ frontend/properties.js
@@ -4,8 +4,8 @@ gOptions = {
   clientUrl: 'http://ec2-18-212-57-171.compute-1.amazonaws.com',
 
   // localhost
-  // serverUrl: 'http://127.0.0.1:3000',
-  // clientUrl: 'http://127.0.0.1/sign',
+  //serverUrl: 'http://localhost:3000',
+  //clientUrl: 'http://localhost/ESS/sign',
 
   serverPort: ':3000'
 }
